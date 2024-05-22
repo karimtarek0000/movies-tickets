4932d1dfe06389b4b66aa22d6c5720ab40a64b33
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2024-05-22 19:50:02 +0300

    Update axios.js

--- src/server/axios.js
+++ src/server/axios.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/https://movies-tickets-api-production.up.railway.app";
+axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/http://movies-tickets-api-production.up.railway.app";
